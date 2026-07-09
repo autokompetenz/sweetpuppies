da4779504d8ec40e0d2620223761b8ef2c02d74c
headshoot17 <djenmarlet@gmail.com>
2026-07-09 12:21:41 +0100

    fix: remove floating contact button from public pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Toast from './components/Toast';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ClientBottomNav from './components/ClientBottomNav';
-import MailButton from './components/MailButton';
 import DeliveryModal from './components/DeliveryModal';
 
 import Home from './pages/Home';
@@ -40,7 +39,6 @@ function PublicLayout({ children }) {
       {children}
       <Footer />
       <ClientBottomNav />
-      <MailButton />
       <DeliveryModal />
       <Toast />
     </>
